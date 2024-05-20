4d813c7b33548f6032791e02c08163b1e7c4cac5
Jahle <Jahle@ABLACKMAN>
2024-05-20 14:32:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+
+namespace Assignment3._1._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetEvenNumbers());
+
+        }
+
+        static string GetEvenNumbers()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 2; i < 100; i += 2)
+            {
+                sb.Append(i + " ");
+            }
+            return
+                sb.ToString();
+        }
+    }
+}
